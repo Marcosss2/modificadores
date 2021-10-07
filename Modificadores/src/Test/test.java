50e790a986d07f9c4160b08c16d7cc6397e04337
Marcosss2 <91058545+Marcosss2@users.noreply.github.com>
2021-10-07 23:44:13 +0200

    n

---
+++ Modificadores/src/Test/test.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Test;
+
+import Clase1.Clase1;
+
+/**
+ *
+ * @author Alumno Mañana
+ */
+public class test {
+  
+      public static void main(String[] args) {
+        // TODO code application logic here
+    
+      Clase1 class1 = new Clase1();
+      
+      System.out.println( class1.atributo);
+     
+      
+      }
+    
+    
+    
+}
---
+++ Modificadores/src/clase3/clase3.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase3;
+
+import Clase1.Clase1;
+
+/**
+ *
+ * @author Alumno Mañana
+ */
+public class clase3  extends Clase1{
+  
+     
+    
+}
